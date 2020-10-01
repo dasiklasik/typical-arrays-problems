85e71cf5fded1cf810219cfb803479a0e968bcec
dasiklasik <daryasamsonowicz@gmail.com>
2020-10-01 19:17:56 +0300

    feat: add max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        let op = 0;
+        let string = array.toString();
+
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] === ',') {
+                sum += parseFloat(string.slice(op, i));
+                op = i + 1;
+
+
+            }
+        }
+
+        sum += parseFloat(string.slice(op, string.length));
+        return sum / array.length;
+    }
 }
